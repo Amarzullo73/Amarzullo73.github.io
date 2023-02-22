bc5876dca9246b52931f1b7ffe959219e4edc33d
Amarzullo73 <94937971+Amarzullo73@users.noreply.github.com>
2023-02-22 15:47:09 -0300

    initial commit
    CV Alejandro Marzullo.

---
+++ java.js
@@ -0,0 +1,16 @@
+
+document.getElementById('boton').onclick = function() {
+
+
+    console.log("Capturamos el evento click");
+    document.getElementById('mensaje').innerHTML = "Mensaje enviado con éxito";
+    document.getElementById('mensaje').style.display='block';
+
+
+}
+
+
+
+
+
+
